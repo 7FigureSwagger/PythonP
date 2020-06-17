eef41083b88678dabb8b915239ecddbd4ea3dace
7figureswagger <swistek.labs@gmail.com>
2020-06-16 23:55:45 -0700

    python basics

---
+++ app.py
@@ -0,0 +1,22 @@
+from math import *
+
+character_name = "Ali"
+character_age = "117"
+is_Male = True
+
+print("This is " + character_name + "!")
+print("He is " + character_age + " years old!")
+
+
+print("\n" + character_name.upper())
+length = len(character_name)
+print("length of " + character_name + " is : " + str(length))
+print(character_name[0])
+print(character_name.index("i"))
+print("Oh no my name changed to " + character_name.replace("Ali", "Ghost"))
+print("Dont worry my name is still " + character_name)
+
+
+print(2 * 8.234)
+my_num = 87 * 9
+print(sqrt(my_num))
