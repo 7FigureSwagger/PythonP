676c13869626a1dfa8b9041b28b26c9c641c77ce
7figureswagger <swistek.labs@gmail.com>
2020-06-17 16:47:37 -0700

    Tuples

---
+++ tut7.py
@@ -0,0 +1,10 @@
+#Tuples
+#Tuples are immutable lists, cast using () instead of [] 
+#Used for when you do not want the data modified
+
+coordinates = (4, 5)
+print(coordinates[0])
+
+#can have lists of Tuples
+places = [(4, 5), (9, 10), (7, 8)]
+print(places)
