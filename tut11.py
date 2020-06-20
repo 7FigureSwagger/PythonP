7d0cc23b18f9ee85994387d71d5bd51db2694cf1
7figureswagger <swistek.labs@gmail.com>
2020-06-20 00:34:40 -0700

    second calculator

--- tut11.py
+++ tut11.py
@@ -5,3 +5,5 @@ def max_num(num1, num2, num3):
     return num2
   else:
     return num3
+
+print(max_num(3, 4, 5))
---
+++ tut12.py
@@ -0,0 +1,3 @@
+num1 = float(input("Enter first number: "))
+op =  input("Input an operator: ")
+num3  = float(input("Enter second number: "))
