c44b716fcc40ce2831ca8c569dee64277df5b8a3
7figureswagger <swistek.labs@gmail.com>
2020-06-20 23:51:59 -0700

    Guessing Game

--- tut15.py
+++ tut15.py
@@ -1,8 +1,18 @@
 
 secret_word = "giraffe"
 guess = ""
+guess_counter = 0
+guess_limit = 3
+out_of_guesses = False
  
-while guess != secret_word:
-  guess = input("Please Guess the Word!\n")
+while guess != secret_word and not(out_of_guesses):
+  if guess_counter < guess_limit:
+    guess = input("Please Guess the Word!\n")
+    guess_counter += 1
+  else:
+    out_of_guesses = True
 
-print("You win!")
+if out_of_guesses:
+  print("Out of guesses, You lose!")
+else:
+  print("You win!")
