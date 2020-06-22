ff496eb180e461ae610b3466092d89a2d099e5f2
7figureswagger <swistek.labs@gmail.com>
2020-06-21 17:41:14 -0700

    For Loops

---
+++ tut16.py
@@ -0,0 +1,19 @@
+currencyConversion = {
+  "BTC": "Bitcoin",
+  "ETH": "Ethereum",
+  "LINK": "ChainLink",
+  "UBT": "Unibright",
+  "EWT": "Energy Web Token"
+}
+
+for key in currencyConversion:
+  print(key)
+
+for key in range(len(currencyConversion)):
+  print(key)
+
+friends = ["Jon", "Jacob", "Jim"]
+
+#Grabbing the value of 3 from the length of the friends array
+for index in range(len(friends)): 
+  print(friends[index])
