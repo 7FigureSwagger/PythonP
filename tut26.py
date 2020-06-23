2ebd5d4416b9d0f6cb3ed0c80b34ee038a540f44
7figureswagger <swistek.labs@gmail.com>
2020-06-23 03:22:23 -0700

    inheritance

--- tut26.py
+++ tut26.py
@@ -3,7 +3,7 @@ from student import Student
 student1 = Student("Oscar", "Accounting", 3.1, False)
 student2 = Student("Jim", "Business", 3.8, False)
 
-#call class function
+#call class function, should be giving/modifying informatio about the class
 
 print("Student 1: " + str(student1.on_honor_roll()))
 print("\nStudent 2: " + str(student2.on_honor_roll()))
