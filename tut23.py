2f48a0486e9183605edc1d87462885e1537c4fe1
7figureswagger <swistek.labs@gmail.com>
2020-06-23 02:18:15 -0700

    modules and pip

--- tut23.py
+++ tut23.py
@@ -1,2 +1,4 @@
 import useful_tools
 
+print(useful_tools.roll_dice(10))
+
--- useful_tools.py
+++ useful_tools.py
@@ -1,7 +1,7 @@
 import random
 feet_in_mile = 5280
 meters_in_kilometer = 1000
-beatles = ["John Lennon". "Paul McCartney", "George Harrison". "Ringo Star"]
+beatles = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Star"]
 
 def get_file_ext(filename):
   return filename[filename.index(".") + 1:]
